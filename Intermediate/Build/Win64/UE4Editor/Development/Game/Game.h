88fed6a200dbc58a7badeaa9eca7930b0ce1cd87
jørn ytterdal <out_now@hotmail.com>
2017-04-05 07:17:20 +0200

    Added AI
    Fikk ikke til animasjon. Burp

---
+++ Intermediate/Build/Win64/UE4Editor/Development/Game/Game.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_GAME_H__
+#define __AUTO_GAME_H__
+//Last Write: 04.04.2017 16.41.00 +00:00
+#include "C:\Users\out_n\Documents\Unreal Projects\Faun\Source\Game\Game.h"
+#endif//__AUTO_GAME_H__
